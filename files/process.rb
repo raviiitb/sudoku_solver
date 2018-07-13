c694bd13ae76a752d500b15f2eeaa2de7da5babf
Ravi Kumar <kumar.ravi.ec@gmail.com>
2018-07-13 04:52:16 +0200

    Added comment messages in code

--- files/process.rb
+++ files/process.rb
@@ -2,23 +2,23 @@ module Files
   # Read Sudoku problem from a file and Write Sudoku solution in a file
   class Process
     class << self
+    	# Read the input file
       def read(file)
         input_arr = []
-        # Read the input file if given from cli else read the input.txt file
         File.open(file).each do |line|
           row = line.gsub(/\s/, '').chomp.split ''
-          # Abort if a given sudoku row has not equal to 9 entries
+          # raise an error if a given sudoku row has not equal to 9 entries
           raise ArgumentError, 'Given input is invalid' if row.size != 9
           # Create a linear input array of all prefilled numbers
           input_arr += row
         end
-        # Abort if a given sudoku has not equal to 81 entries
+        # raise an error if a given sudoku has not equal to 81 entries
         raise ArgumentError, 'Given input is invalid' if input_arr.size != 81
         input_arr
       end
 
+      # Write the output to file in 9x9 format
       def write(arr, file)
-        # Write the output to file in 9x9 format
         File.open(file, 'w') do |f|
           (0..8).each do |row|
             f.puts arr[row * 9, 9].join "\t"
--- main.rb
+++ main.rb
@@ -1,15 +1,19 @@
 require_relative 'sudoku/problem'
 require_relative 'files/process'
 
+# Read the input file if given from cli else read the default files/input.txt
 input_arr = Files::Process.read(ARGV[0] || 'files/input.txt')
+# Initializes an instance of Sudoku::Problem with input_arr as attribute
 problem = Sudoku::Problem.new input_arr
 
-# Create a linear output array of solved sudoku
+# Creates a linear output array of solved sudoku
 output_arr = problem.solve
 
 if output_arr
+  # Compose the solution to a file in files/output.txt
   Files::Process.write output_arr, 'files/output.txt'
   puts 'Solved Sudoku problem is saved in files/output.txt'
 else
+  # Solution does not exists of a given Sudoku problem
   puts 'Given Sudoku problem is invalid'
 end
